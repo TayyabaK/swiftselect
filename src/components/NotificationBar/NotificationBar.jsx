9f16a98a5ded072fce4aab62a2935442b8b9db4c
Tayyaba Khalil <tayyaba.khalil@gmail.com>
2023-09-04 21:58:31 +0300

    Initial UI

--- src/components/NotificationBar/NotificationBar.jsx
+++ src/components/NotificationBar/NotificationBar.jsx
@@ -6,12 +6,10 @@ const NotificationBar = () => {
 
     return (
         <Box className="notification-bar" padding={'6px'}>
-            <Flex width={{base: "50%",sm: "55%", md: "60%", lg: "70%", xl:"62%"}} justifyContent={"end"}>
-                 <p className="notification-text">70% discount - Free shipping worldwide</p>
-            </Flex>
-            <Flex>
-            <RxCross2 color="#fff" fontWeight={900} />
-            </Flex>
+        
+            <p className="notification-text">70% discount - Free shipping worldwide</p>
+
+            <RxCross2 color="#fff" fontWeight={900}/>
         </Box>
     );
 }
