b668d6e7c2734d2adf70394dfb46132d775d2560
Tayyaba Khalil <tayyaba.khalil@gmail.com>
2023-09-08 19:06:23 +0300

    Added ImagePrompt

--- src/components/ImagePrompt/ImagePrompt.jsx
+++ src/components/ImagePrompt/ImagePrompt.jsx
@@ -1,7 +1,5 @@
-
 import React from "react";
-import "./style.css";
-import { Button } from "@chakra-ui/react";
+import { Box, Heading } from "@chakra-ui/react";
 import {FiArrowRight} from "react-icons/fi";
 
 import image from "../../public/wrist-watch.png"
@@ -9,17 +7,24 @@ import image from "../../public/wrist-watch.png"
 export const HeroSection = () => {
 
   return (
-    <div class="container" width={"100%"} >
-          <div>
-              <img src={image} size={"auto"}/>
-          </div>
+    <Box display="flex" position={"relative"} margin={"3rem 3.8rem 3rem 3.8rem"} height={"50%"}>
+          <Box>
+              <img src={image} />
+          </Box>
 
-        <div className="slider-section">
-            <h3>AirNags®</h3>
-            <p>Keep your everyday style chic and on-trend with our selection 20+ styles to choose from.</p>
-            <Button className="click-text">See Collection<FiArrowRight/></Button>
-        </div>
-    </div>
+        <Box zIndex={1}  position={"absolute"} margin={"100px 800px 160px 52px"}>
+            <Heading fontFamily={"Space Grotesk"} 
+            fontSize={"2rem"} fontWeight={"bold"}
+            >
+              RUN SMARTER +
+              </Heading>
+            <Box margin={"1rem 0rem 1rem 0rem"}>
+
+              <p fontFamily="Inter">Phosfluor escently engage worldwide methodologies with web-enabled process-centric technology.</p>
+            </Box>
+            <Box fontFamily="Space Grotesk" fontSize={"1.5rem"} display={"flex"} alignItems={"center"} fontWeight={"bold"}><u>SHOP NOW</u><FiArrowRight/></Box>
+        </Box>
+    </Box>
   );
 };
 
