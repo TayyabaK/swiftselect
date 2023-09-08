4d04b37ef6b5592b9e03c0c59aec71cb690c5e89
Tayyaba Khalil <tayyaba.khalil@gmail.com>
2023-09-08 17:28:11 +0300

    update hero section

--- src/components/HeroSection/Herosection.jsx
+++ src/components/HeroSection/Herosection.jsx
@@ -7,6 +7,7 @@ import {FiArrowRight} from "react-icons/fi";
 // import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 // import { Carousel } from 'react-responsive-carousel';
 import Carousel from 'react-grid-carousel'
+import { getByDisplayValue } from "@testing-library/react";
 
 
 export const HeroSection = () => {
@@ -14,19 +15,19 @@ export const HeroSection = () => {
   const images = ["main-product.png", "main-product.png", "main-product.png"];
 
   return (
-    <div class="container" >
-        <Carousel loop={"true"} showDots={"true"} autoplay={2000}>
-        {  
-              images.map((image) => (
-                  <Carousel.Item>
-                  <div>
-                      <img src={image}/>
-                  </div>
-                 </Carousel.Item>
-              ))
-        }
-
-        </Carousel>
+      <div className="container" display={"block"}>
+          <Carousel loop={"true"} showDots={"true"} autoplay={2000}>
+          {  
+                images.map((image) => (
+                    <Carousel.Item>
+                    <div>
+                        <img src={image} />
+                    </div>
+                   </Carousel.Item>
+                ))
+          }
+            </Carousel>
+    
         <div className="slider-section">
             <h3>AirNags®</h3>
             <p>Keep your everyday style chic and on-trend with our selection 20+ styles to choose from.</p>
--- src/components/ImagePrompt/ImagePrompt.jsx
+++ src/components/ImagePrompt/ImagePrompt.jsx
@@ -4,38 +4,16 @@ import "./style.css";
 import { Button } from "@chakra-ui/react";
 import {FiArrowRight} from "react-icons/fi";
 
-import Carousel from 'react-grid-carousel'
-
+import image from "../../public/wrist-watch.png"
 
 export const HeroSection = () => {
 
-  const images = ["main-product.png", "main-product.png", "main-product.png"];
-
-  const MyDot = ({ isActive }) => (
-    <span
-      style={{
-        display: 'inline-block',
-        height: isActive ? '8px' : '5px',
-        width: isActive ? '8px' : '5px',
-        background: '#1890ff'
-      }}
-    ></span>
-  )
-
   return (
     <div class="container" width={"100%"} >
+          <div>
+              <img src={image} size={"auto"}/>
+          </div>
 
-        <Carousel loop={"true"} showDots={"true"} autoplay={2000}>
-        {  
-              images.map((image) => (
-                  <Carousel.Item>
-                  <div>
-                      <img src={image} size={"auto"}/>
-                  </div>
-                 </Carousel.Item>
-              ))
-        }
-        </Carousel>
         <div className="slider-section">
             <h3>AirNags®</h3>
             <p>Keep your everyday style chic and on-trend with our selection 20+ styles to choose from.</p>
